f2a731fe8c1e9822bdfc3e6137b4fd8e6c45e6c8
Hlieb Kotin <gleb.kotiun@gmaill.com>
2024-12-25 15:24:13 +0100

    fix mobile menu

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,1 +1,25 @@
-console.log('hero');
+/* Open menu */
+
+const openMobMenu = document.querySelector('.burger-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openMobMenu.addEventListener('click', menuOpener);
+
+function menuOpener() {
+  mobileMenu.classList.add('is-open');
+}
+
+/* Close menu*/
+
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+
+mobileMenu.addEventListener('click', menuClose);
+
+function menuClose(event) {
+  const click = event.target.nodeName;
+  if (click !== 'A' && click !== 'SPAN') {
+    return;
+  }
+
+  mobileMenu.classList.remove('is-open');
+}
