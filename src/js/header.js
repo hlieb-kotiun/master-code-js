cad029eaf6015a02f9a3d328fe58574872c14847
Hlieb Kotin <gleb.kotiun@gmaill.com>
2024-12-25 17:28:39 +0100

    fix desktop header

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,20 @@
-console.log('header');
+const desktopHeaderWrapper = document.querySelector(
+  '.desktop-header-nav-wrapper'
+);
+const desktopHeaderList = document.querySelector('.desktop-header-list');
+
+desktopHeaderWrapper.addEventListener('click', menuControl);
+
+function menuControl(evevt) {
+  const menuClick = evevt.target.nodeName;
+  console.log(menuClick);
+
+  if (menuClick !== 'A' && menuClick !== 'BUTTON') {
+    return;
+  }
+  if (desktopHeaderList.classList.contains('close')) {
+    desktopHeaderList.classList.remove('close');
+  } else {
+    desktopHeaderList.classList.add('close');
+  }
+}
