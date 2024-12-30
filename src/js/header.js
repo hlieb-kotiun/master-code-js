1c4e2b7eba4cf21c9db75a8c507454d09f97317a
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2024-12-30 19:23:26 +0100

    fixedheader menu

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,7 @@ const burgerButton = document.querySelector('.mobile-menu-burger-btn');
 const closeButton = document.querySelector('.mobile-menu-close-btn');
 const menuWrapper = document.querySelector('.mobile-menu-wrapper');
 const menuCloseLink = document.querySelector('.mobile-menu-container');
+
 menuCloseLink.addEventListener('click', event => {
   menuWrapper.classList.remove('is-open');
 });
@@ -29,26 +30,18 @@ function toggleMenu() {
   }
 }
 
-document.addEventListener('click', function (event) {
-  const menuTitle = document.querySelector('.title-menu');
-  const menuWrapper = document.querySelector('.tablet-menu-wrapper');
-  if (event.target === menuTitle || event.target.closest('.tablet-menu-link')) {
-    menuWrapper.classList.toggle('is-open');
-  } else {
-    menuWrapper.classList.remove('is-open');
-  }
+const menuTitle = document.querySelector('.title-menu');
+const menuWrapperTablet = document.querySelector('.tablet-menu-wrapper');
 
-  const menuLinks = document.querySelectorAll('.tablet-menu-link');
-  menuLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-      menuWrapper.classList.add('visually-hidden');
-    });
-  });
+menuTitle.addEventListener('click', function () {
+  menuWrapperTablet.classList.add('is-open');
+  menuWrapperTablet.classList.remove('visually-hidden');
+});
 
-  const menuItems = document.querySelectorAll('.tablet-menu-item');
-  menuItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-      menuWrapper.classList.remove('is-open');
-    });
+const menuLinks = document.querySelectorAll('.tablet-menu-link');
+menuLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    menuWrapperTablet.classList.remove('is-open');
+    menuWrapperTablet.classList.add('visually-hidden');
   });
 });
